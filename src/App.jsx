4ae532460f0690e11e442ff1c0db379605c7d489
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2024-05-04 16:35:13 +0530

    ADD: custom classes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <section className='h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl'>
+      {/* <section className='h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl'>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ipsum aliquid fuga ut illum culpa asperiores corrupti sed magni perferendis est inventore, facilis cum, error maiores optio. Cupiditate, accusantium assumenda.</p>
-      </section>
+      </section> */}
+
+      <div className='card'>This is a card</div>
     </div>
   )
 }
