e6f5ca6f064516804ab7a64961ec9a0ea0f45733
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2024-05-04 16:22:52 +0530

    ADD: Base Classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <section className='h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 sm:p-20'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ipsum aliquid fuga ut illum culpa asperiores corrupti sed magni perferendis est inventore, facilis cum, error maiores optio. Cupiditate, accusantium assumenda.</p>
+      </section>
+    </div>
   )
 }
 
-export default App
+export default App;
