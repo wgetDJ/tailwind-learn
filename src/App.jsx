cde7949210b0cf1e18af7b30c9eda554b3bff8ea
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2024-05-04 16:26:24 +0530

    ADD: Basic customization

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <section className='h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 sm:p-20'>
+      <section className='h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl'>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ipsum aliquid fuga ut illum culpa asperiores corrupti sed magni perferendis est inventore, facilis cum, error maiores optio. Cupiditate, accusantium assumenda.</p>
       </section>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "twitter-blue": "#1DA1f2",
+    },
     extend: {},
   },
   plugins: [],
